f44b3c9fbef9e1391447075a22ffa90c9b00def7
Mari-8 <ryanaltimari8@gmail.com>
2021-02-28 17:17:35 -0600

    commit tree 8cbf25f988602f0b2d2e8ddb9710227a9ac26749

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const leastFavoriteCustomer = "Ryan";
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase(); 
+    return customerName
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    const besCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "different customer";
+}
